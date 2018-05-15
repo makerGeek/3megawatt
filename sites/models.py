4d8bab615b7b203f987d2f9ec77a568d487a46ad
makerGeek <moetez.93@gmail.com>
2018-05-15 16:19:05 +0100

    added __str__ to sites.models

--- sites/models.py
+++ sites/models.py
@@ -6,8 +6,14 @@ from datetime import date
 class Site(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "%d - %s"%(self.pk,self.name)
+
 class Operation(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     date = models.DateField(default=date.today)
     a_value = models.DecimalField(max_digits=30, decimal_places=2)
     b_value = models.DecimalField(max_digits=30, decimal_places=2)
+
+    def __str__(self):
+        return "%d - %s %s"%(self.pk,self.site.name, self.date)
