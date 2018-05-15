ed173c2c4677d1158b1967db403072fd72244880
makerGeek <moetez.93@gmail.com>
2018-05-15 16:42:40 +0100

    created /sites/site_id page

--- sites/urls.py
+++ sites/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
     path('', SitesList.as_view()),
     path('sites/', SitesList.as_view()),
+    path('sites/<int:site_id>', OperationsList.as_view()),
 ]
--- sites/views.py
+++ sites/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from.models import Site
+from.models import Site, Operation
 # Create your views here.
 from django.views.generic import ListView
 
@@ -7,3 +7,17 @@ from django.views.generic import ListView
 class SitesList(ListView):
     template_name = 'sites/sites_list.html'
     model = Site
+
+class OperationsList(ListView):
+    template_name = 'sites/operations_list.html'
+    model = Operation
+
+    def get_queryset(self):
+        site_id = self.kwargs['site_id']
+        return Operation.objects.filter(site_id=site_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        site_id = self.kwargs['site_id']
+        context['site_name'] = Site.objects.filter(id=site_id).first().name
+        return context
