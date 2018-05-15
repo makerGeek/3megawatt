57aeb58209936bd158caacee896899f2da4772fe
makerGeek <moetez.93@gmail.com>
2018-05-15 18:22:37 +0100

    added url names

--- sites/urls.py
+++ sites/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', SitesList.as_view()),
-    path('sites/', SitesList.as_view()),
-    path('sites/<int:site_id>', OperationsList.as_view()),
-    path('summary/', Summary.as_view()),
-    path('summary-average', SummaryAverage.as_view()),
+    path('sites/', SitesList.as_view(), name='sites'),
+    path('sites/<int:site_id>', OperationsList.as_view(), name='operations_list'),
+    path('summary/', Summary.as_view(), name='summary'),
+    path('summary-average', SummaryAverage.as_view(), name='summary-average'),
 ]
