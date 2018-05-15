f8914328657c9ff8c4a5e57e0a6d5141834f885b
makerGeek <moetez.93@gmail.com>
2018-05-15 16:14:40 +0100

    created sites.models.Site and sites.models.Operation and their migrations

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+
+class Site(models.Model):
+    name = models.CharField(max_length=200)
+
+class Operation(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
+    a_value = models.DecimalField(max_digits=30, decimal_places=2)
+    b_value = models.DecimalField(max_digits=30, decimal_places=2)
